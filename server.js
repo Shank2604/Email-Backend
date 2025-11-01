016fb9d5293cd274e7fee1449b73e7442fad94c5
Shashank <unique.shashank02@gmail.com>
2025-11-01 08:17:18 +0530

    cors whitelist added

--- server.js
+++ server.js
@@ -43,10 +43,24 @@ try{
   console.error("Error configuring Brevo client:", err);
 }
 
+const whitelist = [
+  //your frontend url;
+];
 
+const corsOptions = {
+  origin: function (origin, callback){
+    if (whitelist.indexOf(origin) !== -1 || !origin){
+      callback(null, true);
+    }else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
